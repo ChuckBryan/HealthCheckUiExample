e18f29609dc405ace572be15e3fee24d08582889
Chuck Bryan <cbryan@marathonus.com>
2023-09-05 07:03:28 -0400

    Add Health Check UI

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,26 @@
+
+
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHealthChecks()
+    .AddRabbitMQ(rabbitConnectionString: "amqp://localhost:5672", tags: new string[] { "rabbitmq" });
+
+builder.Services.AddHealthChecksUI().AddInMemoryStorage();
+
 var app = builder.Build();
 
+app.UseHealthChecksUI(config => config.UIPath = "/hc-ui");
+
+app.MapHealthChecks("/health", new HealthCheckOptions
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
+
 app.MapGet("/", () => "Hello World!");
+
+
 
 app.Run();
